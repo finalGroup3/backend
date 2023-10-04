de92b17ecafaf8b93f63624f0d760a3c9c3dd567
LaithShatnawi <laith.sh66@gmail.com>
2023-10-04 20:17:21 +0000

    adding to jwt

--- src/auth/models/users.js
+++ src/auth/models/users.js
@@ -9,12 +9,12 @@ const userModel = (sequelize, DataTypes) => {
   const model = sequelize.define('Users', {
     username: { type: DataTypes.STRING, required: true, unique: true },
     password: { type: DataTypes.STRING, required: true },
-    image:{ type: DataTypes.STRING},
+    image: { type: DataTypes.STRING },
     role: { type: DataTypes.ENUM('user', 'owner', 'admin'), required: true, defaultValue: 'user' },
     token: {
       type: DataTypes.VIRTUAL,
       get() {
-        return jwt.sign({ username: this.username }, SECRET);
+        return jwt.sign({ username: this.username, role: this.role }, SECRET);
       },
       set(tokenObj) {
         let token = jwt.sign(tokenObj, SECRET);
@@ -27,7 +27,7 @@ const userModel = (sequelize, DataTypes) => {
         const acl = {
           user: ['readUser', 'createUser', 'deleteUser'],
           owner: ['readOwner', 'createOwner', 'updateOwner'],
-          admin: ['read', 'create', 'update', 'delete','readOwner', 'createOwner', 'updateOwner','readUser', 'createUser', 'deleteUser']
+          admin: ['read', 'create', 'update', 'delete', 'readOwner', 'createOwner', 'updateOwner', 'readUser', 'createUser', 'deleteUser']
         };
         return acl[this.role];
       }
